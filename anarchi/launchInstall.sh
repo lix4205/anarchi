36dd873cf4035f10f8c89d105d2680fa10a05b97
lix4205 <coutaud.e@gmail.com>
2018-01-29 21:41:07 +0100

    li update

--- anarchi/launchInstall.sh
+++ anarchi/launchInstall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -11,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/bin/bash
    
 DIR_SCRIPTS="$( dirname $0 )" 
 # && [[ "$DIR_SCRIPTS" == "." ]] && DIR_SCRIPTS=$( pwd )
